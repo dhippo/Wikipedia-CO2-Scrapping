bec8b50db6f19c8e2a3a47ef39f32e76ae853f6e
hippolyte <durandhippolyte@gmail.com>
2024-03-04 15:31:34 +0100

    Ajout route CSV et route SUM

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,11 @@
 # app/crud.py
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from . import models
 import requests
 from bs4 import BeautifulSoup
+import csv
+from io import StringIO
 
 def scrape_and_save(db: Session):
     if check_data_exists(db):
@@ -54,3 +57,52 @@ def clean_and_convert_to_float(data_str):
 
 def check_data_exists(db: Session):
     return db.query(models.Emission).count() > 20
+
+
+def scrape_and_save_csv():
+    try:
+        print("Début du scrapping Wikipedia")
+        url = 'https://fr.wikipedia.org/wiki/Gaz_%C3%A0_effet_de_serre'
+        response = requests.get(url)
+        if response.status_code != 200:
+            print("Erreur: Impossible d'accéder à la page Wikipedia")
+            return None, "Erreur de réponse HTTP"
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        content = soup.find('div', id='bodyContent').text
+        start_index = content.find("Approche consommationt CO2/personne")
+        end_index = content.find(
+            "Approche territoriale : les émissions sont attribuées au pays sur le territoire duquel elles se produisent.Approche consommation")
+
+        if start_index == -1 or end_index == -1:
+            print("Erreur: Impossible de trouver les indices de début et de fin dans la page")
+            return None, "Indices de début et de fin non trouvés"
+
+        data = content[start_index:end_index]
+        lines = [line for line in data.split('\n') if line.strip() != '']
+        print(f"Nombre de lignes extraites: {len(lines)}")
+
+        structured_data = structure_raw_data(lines[1:])
+        print("Données structurées créées")
+
+        output = StringIO()
+        writer = csv.writer(output)
+        writer.writerow(['Country Name', 'Territorial Approach', 'Territorial Approach by Inhabitant', 'Consumption Approach'])
+
+        for row in structured_data:
+            writer.writerow(row)
+        print("Données écrites dans le CSV")
+
+        output.seek(0)
+        return output.getvalue(), None
+    except Exception as e:
+        print(f"Erreur lors du scrapping ou de la génération du CSV: {e}")
+        return None, str(e)
+
+
+def get_total_territorial_approach(db: Session):
+    total = db.query(func.sum(models.Emission.territorial_approach)) \
+              .filter(models.Emission.country_name != "Union européenne") \
+              .filter(models.Emission.country_name != "Monde") \
+              .scalar()
+    return total
--- app/routers/scrapping.py
+++ app/routers/scrapping.py
@@ -1,5 +1,6 @@
 # app/routers/scrapping.py
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 from .. import crud
 from ..dependencies import get_db
@@ -12,4 +13,22 @@ def wikipedia_scrapping(db: Session = Depends(get_db)):
     if result == "Données déjà scrappées":
         raise HTTPException(status_code=400, detail=result)
     return {"message": "Scrapping réussi et données sauvegardées."}
+
+
+@router.get("/scrape_and_save_csv")
+def wikipedia_scrapping_csv():
+    csv_data, error = crud.scrape_and_save_csv()
+
+    if error:  # Si une erreur est renvoyée
+        raise HTTPException(status_code=500, detail=f"Erreur importante lors du scrapping ou de la génération du CSV: {error}")
+
+    # Création d'une réponse StreamingResponse pour renvoyer le fichier CSV
+    response = StreamingResponse(iter([csv_data]), media_type="text/csv")
+    response.headers["Content-Disposition"] = "attachment; filename=emissions_data.csv"
+    return response
+
+@router.get("/SUM_total_territorial_approach")
+def total_territorial_approach(db: Session = Depends(get_db)):
+    total = crud.get_total_territorial_approach(db)
+    return {"total_territorial_approach": total}
 
